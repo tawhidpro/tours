524fb870d87279969cad84b5db6a13c05f59bcc1
unknown <towhid2302@gmail.com>
2021-10-30 22:37:58 +0545

    Design completed

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours,removeTour}) => {
+ 
+  return <section>
+    <div className="title">
+      <h2>{tours.length < 0 ? 'No tour place left' : 'our tour'  }</h2>
+      <div className="underlinn"></div>
+    </div>
+    <div className="div">
+      {
+        tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} ></Tour>
+        })
+      }
+    </div>
+  </section>;
 };
 
 export default Tours;
